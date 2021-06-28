b61bbdd3e4289cae89efbd1b2956764b8514ea68
Nardev12 <veksfp@gmail.com>
2021-06-28 12:09:30 +0100

    fix

--- src/pages/ReviewStudent.vue
+++ src/pages/ReviewStudent.vue
@@ -23,10 +23,13 @@
         </div>
       </div>
     </div>
+    <button class="menu-btn" @click="saveStudent()">Spremi</button>
   </div>
 </template>
 
 <script>
+import { db } from "../firebase";
+
 export default {
   data() {
     return {
@@ -37,13 +40,15 @@ export default {
     };
   },
   methods: {
-    saveProfessor() {
-      alert(JSON.stringify(this.form));
+    saveStudent() {
+      db.collection("Student-Review").add({
+        activity: this.form.activity,
+        obligations: this.form.obligations
+
+      })
+  
     },
-    updateRating(rate) {
-      this.form.categories.values = rate;
-      console.log("value", this.form.categories.value);
-    },
+    
   },
 };
 </script>
